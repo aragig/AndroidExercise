564539e17a31c274e51025ef06600e47c98ab23d
Toshihiko Arai <araemonz@gmail.com>
2025-09-27 10:22:16 +0900

    websocket関連の変更

--- TryWebSocketServer/app/build.gradle.kts
+++ TryWebSocketServer/app/build.gradle.kts
@@ -46,5 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("org.java-websocket:Java-WebSocket:1.5.2")
+    implementation(libs.java.websocket)
 }
